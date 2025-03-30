0e6e41827843f549fe2d5052f3b5ad5367973bb4
Gokul M R <gokulmr331@gmail.com>
2025-03-30 23:38:03 +0530

    server and db connection created

---
+++ backend/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDb } from './config/dbConnect';
+
+dotenv.config();
+const app = express();
+
+const Port = process.env.PORT || 3000
+
+const corsOptions = {
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+};
+
+connectDb();
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.listen(Port, () => {
+    console.log('Server is running on port 3000');
+});
