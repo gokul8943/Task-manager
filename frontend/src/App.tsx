ac182352a79672935b52ee8ef515e74b58c5005f
Gokul M R <gokulmr331@gmail.com>
2025-04-02 12:45:53 +0530

    routes added in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import {Routes,Route} from 'react-router-dom'
+import LoginPage from './pages/user/LoginPage'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   )
 }
